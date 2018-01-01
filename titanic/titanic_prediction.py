35aa6d5d2b6ccf48066d4608456d02733bc78157
Nabil <nabillionaire@live.com>
2017-12-31 23:48:42 -0500

    Adding first basic model, key features missing but it's a start. Need to quantize the dropped features

--- titanic/titanic_prediction.py
+++ titanic/titanic_prediction.py
@@ -1,20 +1,108 @@
+'''
+OK to compute....	    Nominal	Ordinal	Interval Ratio
+frequency distribution.	Yes	    Yes	    Yes	     Yes
+median and percentiles.	No	    Yes	    Yes	     Yes
+add or subtract.	    No	    No	    Yes	     Yes
+mean, standard deviation, standard error of the mean.	No	No	Yes	Yes
+ratio, or coefficient of variation.	No	No	No	Yes
+ 	 	 	 	 
+'''
+
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
+from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier
 
-train_df = pd.read_csv('./datasets/train.csv')
-test_df = pd.read_csv('./datasets/test.csv')
-df = [train_df, test_df]
+df = {
+    'train_df': pd.read_csv('./datasets/train.csv'),
+    'test_df': pd.read_csv('./datasets/test.csv')
+}
+
+def preview():
+    '''
+    Preview the data
+    '''
+    # Display columns
+    print('Train columns:', df['train_df'].columns.values.tolist())
+    print('Test columns:', df['test_df'].columns.values.tolist())
+    print(100 * '-')
+    # Preview
+    print(df['train_df'].head())
+    print(df['train_df'].describe())
+
+    # Females more likely to survive than males
+    print(df['train_df'][['Sex', 'Survived']].groupby(['Sex']).mean().sort_values(by='Survived', ascending=False))
+    # Class 1 most likely to survive, followed by 2 and 3
+    print(df['train_df'][['Pclass', 'Survived']].groupby(['Pclass']).mean().sort_values(by='Survived', ascending=False))
+    # 1 > 2 > 0 > 3
+    print(df['train_df'][['SibSp', 'Survived']].groupby(['SibSp']).mean().sort_values(by='Survived', ascending=False))
+    # 3 > 1 > 2 > 0
+    print(df['train_df'][['Parch', 'Survived']].groupby(['Parch']).mean().sort_values(by='Survived', ascending=False))
+    # C > Q > S
+    print(df['train_df'][['Embarked', 'Survived']].groupby(['Embarked']).mean().sort_values(by='Survived', ascending=False))
+    # Drop cabin
+    # print(train_df[['Cabin', 'Survived']].groupby(['Cabin']).mean().sort_values(by='Survived', ascending=False))
+
+def visualize():
+    '''
+    Visualize Age, Ticket, Fare
+    '''
+    # Certain age bands more likely to survive
+    g_age = sns.FacetGrid(df['train_df'], col='Survived')
+    g_age.map(plt.hist, 'Age', bins=20)
+    plt.show()
+
+    grid = sns.FacetGrid(df['train_df'], col='Survived')
+    grid.map(plt.hist, 'Fare', bins=20)
+    plt.show()
+
+    grid = sns.FacetGrid(df['train_df'], row='Embarked', col='Survived', size=2.2, aspect=1.6)
+    grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
+    grid.add_legend()
+    plt.show()
+
+def check_title():
+    '''
+    Extract Title from Name
+    '''
+    # Confirmed Title as an useful feature, let's keep it (after some processing)
+    train_df2 = df['train_df'].copy()
+    train_df2['Title'] = train_df2['Name'].str.extract('([A-Za-z]+\.)', expand=False)
+    print(train_df2[['Title', 'Survived']].groupby(['Title']).agg(['mean', 'count']).sort_values(by=[('Survived', 'mean')], ascending=False))
 
 '''
-Preview the data
+Processing
 '''
-# Display columns
-print('Train columns:', train_df.columns.values.tolist())
-print('Test columns:', test_df.columns.values.tolist())
-print(100 * '-')
-# Preview
-print(train_df.head())
+# Do one-hot encoding for all categorical features (using dummy vars)
+# Maybe combine SibSp and Parch into single feature?
 
+for key in df.keys():
+    df[key]['Title'] = df[key]['Name'].str.extract('([A-Za-z]+\.)', expand=False)
+    df[key].drop(['Name'], axis=1, inplace=True)
+
+for key in df.keys():
+    df[key] = pd.get_dummies(df[key], columns=['Sex', 'Embarked', 'Pclass'])
+    df[key].drop(['Cabin', 'Ticket', 'PassengerId', 'SibSp', 'Parch', 'Age', 'Fare', 'Title'], axis=1, inplace=True) # Also drop Cabin as we won't use it
+
+
+[print(data.head(), '\n', '-' * 100) for data in df.values()]
+# [data.fillna(0) for data in df.values()]
 '''
-Preprocessing
+Prediction
 '''
+X_train = df['train_df'].drop('Survived', axis=1)
+Y_train = df['train_df']['Survived']
+X_test = df['test_df'].copy()
+
+logreg = LogisticRegression()
+logreg.fit(X_train, Y_train)
+Y_pred = logreg.predict(X_test)
+acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
+print(acc_log)
+
+perceptron = Perceptron()
+perceptron.fit(X_train, Y_train)
+Y_pred = perceptron.predict(X_test)
+acc_perceptron = round(perceptron.score(X_train, Y_train) * 100, 2)
+print(acc_perceptron)
--- titanic/titanic_tutorial/titanic_tutorial.py
+++ titanic/titanic_tutorial/titanic_tutorial.py
@@ -1,3 +1,23 @@
+'''
+Workflow stages
+1) Question or problem definition.
+2) Acquire training and testing data.
+3) Wrangle, prepare, cleanse the data.
+4) Analyze, identify patterns, and explore the data.
+5) Model, predict and solve the problem.
+6) Visualize, report, and present the problem solving steps and final solution.
+7) Supply or submit the results.
+
+Workflow goals
+1) Classifying. We may want to classify or categorize our samples. We may also want to understand the implications or correlation of different classes with our solution goal.
+2) Correlating. One can approach the problem based on available features within the training dataset. Which features within the dataset contribute significantly to our solution goal? Statistically speaking is there a correlation among a feature and solution goal? As the feature values change does the solution state change as well, and visa-versa? This can be tested both for numerical and categorical features in the given dataset. We may also want to determine correlation among features other than survival for subsequent goals and workflow stages. Correlating certain features may help in creating, completing, or correcting features.
+3) Converting. For modeling stage, one needs to prepare the data. Depending on the choice of model algorithm one may require all features to be converted to numerical equivalent values. So for instance converting text categorical values to numeric values.
+4) Completing. Data preparation may also require us to estimate any missing values within a feature. Model algorithms may work best when there are no missing values.
+5) Correcting. We may also analyze the given training dataset for errors or possibly innacurate values within features and try to corrent these values or exclude the samples containing the errors. One way to do this is to detect any outliers among our samples or features. We may also completely discard a feature if it is not contribting to the analysis or may significantly skew the results.
+6) Creating. Can we create new features based on an existing feature or a set of features, such that the new feature follows the correlation, conversion, completeness goals.
+7) Charting. How to select the right visualization plots and charts depending on nature of the data and the solution goals.
+'''
+
 import pandas as pd
 import numpy as np
 import random as rnd
