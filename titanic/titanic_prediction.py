79c03833a130cd2802c69ecb9a33e98c57e28954
Nabil <nabillionaire@live.com>
2018-01-01 19:45:12 -0500

    Extracting title feature from name, seeing improved scores

--- titanic/titanic_prediction.py
+++ titanic/titanic_prediction.py
@@ -13,11 +13,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 
 df = {
     'train_df': pd.read_csv('./datasets/train.csv'),
     'test_df': pd.read_csv('./datasets/test.csv')
 }
+
 
 def preview():
     '''
@@ -44,6 +50,7 @@ def preview():
     # Drop cabin
     # print(train_df[['Cabin', 'Survived']].groupby(['Cabin']).mean().sort_values(by='Survived', ascending=False))
 
+
 def visualize():
     '''
     Visualize Age, Ticket, Fare
@@ -62,14 +69,42 @@ def visualize():
     grid.add_legend()
     plt.show()
 
-def check_title():
+
+def check_title(processed=False):
     '''
     Extract Title from Name
     '''
     # Confirmed Title as an useful feature, let's keep it (after some processing)
+    if processed:
+        print(df['train_df'][['Title', 'Survived']].groupby(['Title']).agg(['mean', 'count']).sort_values(
+            by=[('Survived', 'mean')], ascending=False))
+        return
     train_df2 = df['train_df'].copy()
     train_df2['Title'] = train_df2['Name'].str.extract('([A-Za-z]+\.)', expand=False)
     print(train_df2[['Title', 'Survived']].groupby(['Title']).agg(['mean', 'count']).sort_values(by=[('Survived', 'mean')], ascending=False))
+
+
+def rank_algos(X_train, Y_train, X_test):
+    classifiers = {'logreg': LogisticRegression(), 'svc': SVC(), 'knn': KNeighborsClassifier(),
+                   'gaussian': GaussianNB(), 'perceptron': Perceptron(), 'linear_svc': LinearSVC(),
+                   'sgd': SGDClassifier(), 'decision_tree': DecisionTreeClassifier(),
+                   'random_forest': RandomForestClassifier(n_estimators=100),
+    }
+    predictions = {}
+    scores = {}
+
+    for classifier_key in classifiers:
+        clf = classifiers[classifier_key]
+        clf.fit(X_train, Y_train)
+        predictions[classifier_key] = clf.predict(X_test)
+        scores[classifier_key] = round(clf.score(X_train, Y_train) * 100, 2)
+
+    models = pd.DataFrame({
+        'Score': scores
+    })
+
+    print(models.sort_values(by='Score', ascending=False))
+
 
 '''
 Processing
@@ -77,17 +112,23 @@ Processing
 # Do one-hot encoding for all categorical features (using dummy vars)
 # Maybe combine SibSp and Parch into single feature?
 
-for key in df.keys():
-    df[key]['Title'] = df[key]['Name'].str.extract('([A-Za-z]+\.)', expand=False)
+for key in df:
+    df[key]['Title'] = df[key]['Name'].str.extract('([A-Za-z]+)\.', expand=False)
     df[key].drop(['Name'], axis=1, inplace=True)
 
-for key in df.keys():
-    df[key] = pd.get_dummies(df[key], columns=['Sex', 'Embarked', 'Pclass'])
-    df[key].drop(['Cabin', 'Ticket', 'PassengerId', 'SibSp', 'Parch', 'Age', 'Fare', 'Title'], axis=1, inplace=True) # Also drop Cabin as we won't use it
+for data in df.values():
+    data.replace(['Ms', 'Mme', 'Mlle'], 'Miss', inplace=True)
+    data.replace(['Countess', 'Lady', 'Sir'], 'Royal', inplace=True)
+    data.replace(['Col', 'Major'], 'Army', inplace=True)
+    data.replace(['Capt', 'Don', 'Jonkheer', 'Rev'], 'Rare', inplace=True)
 
+for key in df:
+    df[key] = pd.get_dummies(df[key], columns=['Sex', 'Embarked', 'Pclass', 'Title'])
+    df[key].drop(['Cabin', 'Ticket', 'PassengerId', 'SibSp', 'Parch', 'Fare', 'Age'], axis=1, inplace=True) # Also drop Cabin as we won't use it
 
-[print(data.head(), '\n', '-' * 100) for data in df.values()]
-# [data.fillna(0) for data in df.values()]
+df['train_df'], df['test_df'] = df['train_df'].align(df['test_df'], join='outer', axis=1, fill_value=0)
+df['test_df'].drop('Survived', axis=1, inplace=True)
+
 '''
 Prediction
 '''
@@ -95,14 +136,4 @@ X_train = df['train_df'].drop('Survived', axis=1)
 Y_train = df['train_df']['Survived']
 X_test = df['test_df'].copy()
 
-logreg = LogisticRegression()
-logreg.fit(X_train, Y_train)
-Y_pred = logreg.predict(X_test)
-acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
-print(acc_log)
-
-perceptron = Perceptron()
-perceptron.fit(X_train, Y_train)
-Y_pred = perceptron.predict(X_test)
-acc_perceptron = round(perceptron.score(X_train, Y_train) * 100, 2)
-print(acc_perceptron)
+rank_algos(X_train, Y_train, X_test)
